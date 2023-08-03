eb86a4b14d80a8c1e95e96c8896600cab68b99be
Vanshika-026 <vanshikachau26@gmail.com>
2023-08-03 13:07:16 +0530

    variables & datatypes

---
+++ datatype.js
@@ -0,0 +1,16 @@
+let studentName1 = "Riya";
+let studentName2 = "Rajan";
+let studentName3 = "Rati";
+
+document.getElementById("demo").innerHTML = studentName1 + "<br>" +  studentName2 + "<br>" +  studentName3; 
+
+
+let x1 = 28;
+let x2 = 28;
+let x3 = 30;
+document.getElementById("demo2").innerHTML = x1 + "<br>" +  x2 + "<br>" +  x3; 
+
+document.getElementById("demo3").innerHTML =  (x1 == x2) + "<br>" + (x2 == x3) ; 
+
+
+
