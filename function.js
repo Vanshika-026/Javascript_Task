09272537dd7392fe0c125cab40870b3c565fbcd2
VanshikaChaudhary <vanshikachau26@gmail.com>
2023-08-04 17:25:26 +0530

    fetch

---
+++ function.js
@@ -0,0 +1,16 @@
+let x = myfunction(8, 4);
+document.getElementById("demo").innerHTML = x;
+
+function myfunction(a, b){
+    return a * b;
+}
+
+// let x = myfunction(8, 8);
+// function myfunction(a, b){
+// console.log(a * b)
+// }
+
+// let x = myfunction(18, 9);
+// function myfunction(a, b){
+//     console.log(a - b)
+// }
