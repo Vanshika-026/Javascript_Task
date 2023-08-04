f6c0938f8c59203ac565362cbc2becce3fbccda3
VanshikaChaudhary <vanshikachau26@gmail.com>
2023-08-04 11:28:27 +0530

    loop & array

---
+++ loop.js
@@ -0,0 +1,15 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(arr);
+
+for( i=0; i<arr.length ; i++){
+    console.log(arr[i]);
+}
+ let arr1 = 0;
+ do{
+    console.log("arr1: " + arr1);
+    arr1++;
+ }while (arr1 < 4);
+
+
+
+
